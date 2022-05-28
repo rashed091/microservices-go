940813e89df0d3adf638d3bff61bb99fc0c7dd9a
mrasheduzzaman <rashed091nsu@gmail.com>
2022-05-28 18:10:50 +0600

    Start a basic server

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -8,7 +10,19 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("Hello, world!")
-	});
+		d, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Ooops", http.StatusBadRequest)
+			return
+		}
+
+		log.Printf("Data %s\n", d)
+		fmt.Fprintf(w, "Hello %s", d)
+	})
+
+	http.HandleFunc("/goodbye", func(w http.ResponseWriter, r *http.Request) {
+		log.Panicln("Goodbye world!")
+	})
 
 	http.ListenAndServe(":9090", nil)
 }
